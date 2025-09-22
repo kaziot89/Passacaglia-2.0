cc3696021f7b72ce67b1e09cbf5c171c709c1946
Kazimierz Targowski <k.targowski@gmail.com>
2025-09-22 21:56:29 +0200

    Fix base for GitHub Pages

--- passacaglia/vite.config.js
+++ passacaglia/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+// automatycznie ustawia base w zależności od środowiska
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/Passacaglia-2.0/", // dokładnie tak samo jak w homepage
-});
+  base: mode === "production" ? "/Passacaglia-2.0/" : "/",
+}));
