684423d57f09497d448771c064848ab85dedc4d5
Kazimierz Targowski <k.targowski@gmail.com>
2025-09-22 21:43:15 +0200

    Przygotowanie do GitHub Pages

--- passacaglia/vite.config.js
+++ passacaglia/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Passacaglia-2.0/", // nazwa repozytorium na GitHubie
+  base: "/Passacaglia-2.0/", // dokładnie tak samo jak w homepage
 });
