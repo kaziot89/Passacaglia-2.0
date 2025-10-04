ce72421d007fa1a6c55bf4079b8cdd5f5375baa5
jwojszczyk <jwojszczyk@gmail.com>
2025-10-04 22:04:09 +0200

    projekty page

--- passacaglia/src/pages/Projekty.jsx
+++ passacaglia/src/pages/Projekty.jsx
@@ -1,36 +1,91 @@
 import React from "react";
 import Logo_web from "../components/Logo_web";
 import Menu_Web from "../components/Menu_web";
-import Menu_Mobile from "../components/Menu_Mobile";
 import Banner_podstrona from "../components/Banner_podstrona";
-import projektyImg from "../Pictures/banner1.jpg";
+import projektyImg from "../Pictures/banner_projekty.jpg"
+import Jesienimg from "../Pictures/jesienzfletem.jpg";
 import Footer from "../components/Footer_web";
 import "../Css/Projekty.css";
-import "../Css/Button.css";
 
 const Projekty = () => {
   return (
     <div>
       <Logo_web />
       <Menu_Web />
-      <Menu_Mobile />
-      <Banner_podstrona image={projektyImg} objectPosition="center 70%" />
+      <Banner_podstrona
+        image={projektyImg}
+        style={{ backgroundPosition: "-0px -362px",
+    backgroundSize: "1492px 2070px"}} 
+        />
+
       <div id="projekty_page">
         <div id="aktualne_projekty">
+        
           <h1>
             AKTUALNE PROJEKTY<div id="line"></div>
           </h1>
 
-          <div id="projekty_aktualne_container"></div>
+          <div id="projekty_aktualne_section">
+            <div className="projekty_container_act">
+              <div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div>
+              <div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div id="archiwalne_projekty">
-          <h1>
-            ARCHIWALNE PROJEKTY<div id="line"></div>
-          </h1>
-          <div id="projekty_archiwalne_container"></div>
+          <h1>ARCHIWALNE PROJEKTY<div id="line"></div></h1>
+          <div id="projekty_archiwalne_section">
+            <div className="projekty_container_arc"><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div><div className="projekty_item">
+                <div className="projekty_photo" style={{
+                backgroundImage: `url(${Jesienimg})`,
+                }}><a href="https://example.com" target="_blank" rel="noopener noreferrer" className="projekty_text_link  projekty_text"><h5><i>Jesień z muzyką dawną</i><br/>II edycja</h5></a>
+                </div>
+              </div>
+            </div>
+          </div>   
         </div>
-
-        <Footer />
+      
+      <Footer />
       </div>
     </div>
   );
