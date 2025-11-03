d59020a89ca8df3e0526811d42d27632d9d2eadd
Kazimierz Targowski <k.targowski@gmail.com>
2025-11-03 22:53:43 +0100

    misja rwa and dots

--- passacaglia/src/components/Banner_podstrona.jsx
+++ passacaglia/src/components/Banner_podstrona.jsx
@@ -1,28 +1,26 @@
 import React from "react";
+import "../Css/Banner_podstrona.css";
 
 const Banner_podstrona = ({ image, objectPosition = "center", style }) => {
   return (
     <div
-      style={{
-        position: "relative",
-        width: "100%",
-        minWidth: "1024px",
-        maxWidth: "1440px",
-        aspectRatio: "1440 / 385",
-        margin: "auto",
-        overflow: "hidden",
-        ...style,
-      }}
+      className="banner_podstrona"
+      // style={{
+      //   position: "relative",
+      //   width: "100%",
+      //   // minWidth: "1024px",
+      //   maxWidth: "1440px",
+      //   aspectRatio: "1440 / 385",
+      //   margin: "auto",
+      //   overflow: "hidden",
+      //   ...style,
+      // }}
     >
       <img
         src={image}
         alt=""
         style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          objectPosition: objectPosition, // ← tu używasz propa
-          display: "block",
+          objectPosition: objectPosition,
         }}
       />
     </div>
